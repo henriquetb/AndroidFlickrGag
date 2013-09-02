c887adf6b8d13cc64bc44fc44cc91820ecae80f0
Henrique Teske Barbieri <henriquetb@gmail.com>
2013-09-02 20:55:48 +1000

    Attributes visibility changed

--- src/com/iseepareidolia/androidflickrgag/Post.java
+++ src/com/iseepareidolia/androidflickrgag/Post.java
@@ -1,15 +1,18 @@
 package com.iseepareidolia.androidflickrgag;
 
 public class Post {
-	int id; 
-	String title;
-	String faceUser; //facebook id of the user who inserted the post
-	String description; 
-	String createdAt; //datetime that the post was created
+	// url to get all posts list
+	private static String URL_ALL_POSTS = "http://futebolsa.org/pareidolia/get_all_posts.php";
+		
+	private int id; 
+	private String title;
+	private String faceUser; //facebook id of the user who inserted the post
+	private String description; 
+	private String createdAt; //datetime that the post was created
 	//flickr variables
-	int flickrFarm;
-	String flickrId;
-	String flickrSecret;
-	int flickrServer;
-	int clicks; //counter-how many times the post was viewed
+	private int flickrFarm;
+	private String flickrId;
+	private String flickrSecret;
+	private int flickrServer;
+	private int clicks; //counter-how many times the post was viewed
 }
